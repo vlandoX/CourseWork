b141d73b4fb12ac897fe2000020341e03ef83c25
Vladislav Filimonov <vlandof@mail.ru>
2023-12-01 15:52:32 +0300

    created ui of signIn and signUp screens

---
+++ app/src/main/java/com/example/coursework/SignUpFragment.kt
@@ -0,0 +1,40 @@
+package com.example.coursework
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.coursework.databinding.FragmentSignUpBinding
+
+
+class SignUpFragment : Fragment() {
+
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+
+        return view
+    }
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+}
